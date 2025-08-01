8f920b32d49299e2c89adc5d2ed1c149aa79abd0
firdavs130828 <arabovfirdavs7@gmail.com>
2025-08-01 12:19:40 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+let a = prompt("String typeda ma'lumot kiriting:");
+let b = +prompt("Number typeda ma'lumot kiriting:");
+let c = prompt("Bolean typeda ma'lumot kiriting:");
+
+confirm("kiritilgan ma'lumotlar to'g'rimi?:");
+alert(`Siz kiritgan birinchi ${a} so'zi ${typeof a} typega tegishli\n Siz kiritgan ${b} ma'lumoti ${typeof b} type ga tegishli\n Siz kiritgan ${c} ma'lumoti boolean type ga tegishli `)
